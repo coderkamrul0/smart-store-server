06f823cf659505215f2cfb17d4635ede041865b7
Kamrul Hasan <kamrul444216@gmail.com>
2023-08-31 18:31:05 +0600

    make single product api

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req,res,next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'Unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err,decoded) => {
+    if(err){
+      return res.status(401).send({error: true, message: 'Unauthorized access'});
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 
 
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mjrrjle.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +45,25 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const usersCollection = client.db('smartshop').collection('users')
+    const productsCollection = client.db('smartshop').collection('products')
+
+
+    // jwt
+    app.post('/jwt', (req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '2h'})
+      res.send({token}) 
+    })
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
 
 
     // save user in database
@@ -45,6 +78,36 @@ async function run() {
         res.send(result)
     })
 
+    // get all users
+    app.get('/users', verifyJWT, verifyAdmin, async(req,res) =>{
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.get('/users/admin/:email', verifyJWT, async(req,res) => {
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        res.send({admin:false})
+      }
+      const query = {email:email};
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role === 'admin'}
+      res.send(result);
+    })
+
+    // get all products
+    app.get('/products', async(req,res)=>{
+      const result = await productsCollection.find().toArray();
+      res.send(result)
+    })
+
+    // get single product
+    app.get('/products/:id',  async(req,res) => {
+      const id = req.params.id;
+      console.log(id);
+      const result = await productsCollection.findOne({_id: new ObjectId(id)})
+      res.send(result)
+    })
 
 
 
