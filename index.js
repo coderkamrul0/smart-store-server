5eb79e31a4509db422d694b692d40e82e590faae
Kamrul Hasan <kamrul444216@gmail.com>
2023-09-01 12:23:09 +0600

    Payment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 // middleware
@@ -104,11 +105,43 @@ async function run() {
     // get single product
     app.get('/products/:id',  async(req,res) => {
       const id = req.params.id;
-      console.log(id);
       const result = await productsCollection.findOne({_id: new ObjectId(id)})
       res.send(result)
     })
 
+
+    // payment api
+    app.post('/api/create-checkout-session', async (req, res) => {
+      try {
+        const { products } = req.body;
+        console.log(products);
+
+        const lineItems = products.map((product) => ({
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: product.name,
+              images: [product.image],
+            },
+            unit_amount: product.price * 100,
+          },
+          quantity: product.quantity,
+        }));
+
+        const session = await stripe.checkout.sessions.create({
+          payment_method_types: ["card"],
+          line_items: lineItems,
+          mode: "payment",
+          success_url: 'http://localhost:5173/success',
+          cancel_url: 'http://localhost:5173/cancel',
+        });
+
+        res.json({ id: session.id });
+      } catch (error) {
+        console.error('Error creating checkout session:', error);
+        res.status(500).json({ error: 'Failed to create checkout session' });
+      }
+    });
 
 
 
